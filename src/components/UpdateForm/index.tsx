ec1e32bbd7693e9494eb5bc0aaf3c1be1c3d9d45
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-01-24 01:19:36 -0500

    update form validated

--- src/components/UpdateForm/index.tsx
+++ src/components/UpdateForm/index.tsx
@@ -1,9 +1,20 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { From, TextInput } from '../../styled-components/forms';
 import user from '../../assets/user.png';
+import { UpdateSchema, UpdateSchemaType } from '../../utils/validateForm';
 
 function UpdateForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UpdateSchemaType>({
+    resolver: zodResolver(UpdateSchema),
+  });
+
   const [img, setImg] = useState<File>();
   const imgRef = useRef<HTMLInputElement>(null);
 
@@ -17,8 +28,12 @@ function UpdateForm() {
     if (img) return URL.createObjectURL(img);
     return user;
   };
+
+  function onsubmit(data: UpdateSchemaType) {
+    console.log(data);
+  }
   return (
-    <From>
+    <From onSubmit={handleSubmit(onsubmit)}>
       <div className="m-auto">
         <input id="file" className="hidden" type="file" ref={imgRef} onChange={handleOnFileChange} />
         <label htmlFor="file">
@@ -27,13 +42,53 @@ function UpdateForm() {
           </div>
         </label>
       </div>
-      <TextInput type="text" value="full name" placeholder="full name" />
-      <TextInput type="text" value="full name" placeholder="username" />
-      <TextInput type="email" value="full name" placeholder="email" />
-      <TextInput type="password" placeholder="password" />
-      <TextInput type="password" placeholder="confirm password" />
+      <div className="formGroup">
+        <TextInput
+          type="text"
+          placeholder="full name"
+          {...register('fullName')}
+          className={`form-control ${errors.fullName ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.fullName?.message}</div>
+      </div>
+      <div className="formGroup">
+        <TextInput
+          type="text"
+          placeholder="username"
+          {...register('userName')}
+          className={`form-control ${errors.userName ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.userName?.message}</div>
+      </div>
+      <div className="formGroup">
+        <TextInput
+          type="email"
+          placeholder="email"
+          {...register('email')}
+          className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.email?.message}</div>
+      </div>
+      <div className="formGroup">
+        <TextInput
+          type="password"
+          placeholder="password"
+          {...register('password')}
+          className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.password?.message}</div>
+      </div>
+      <div className="formGroup">
+        <TextInput
+          type="password"
+          placeholder="confirm password"
+          {...register('confirm_password')}
+          className={`form-control ${errors.confirm_password ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.confirm_password?.message || errors.confirm?.message}</div>
+      </div>
       <button className="w-2/5 p-3 text-xl text-white bg-gray-800 rounded" type="submit">
-        Update
+        Register
       </button>
     </From>
   );
--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -18,7 +18,31 @@ export const LoginSchema = schema.pick({
   password: true,
 });
 
+export const UpdateSchema = z
+  .object({
+    fullName: z.string().min(5, { message: 'Full Name is required and more than 5' }),
+    userName: z.string().min(3, { message: 'UserName is required and more than 3' }),
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z.string(),
+    confirm_password: z.string(),
+  })
+  .superRefine(({ confirm_password, password }, ctx) => {
+    if (confirm_password !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: "Passwords don't match",
+        path: ['confirm'],
+      });
+    }
+    if (password.length < 5 || confirm_password.length < 5) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Password must be more than 5 characters.`,
+        path: ['confirm'],
+      });
+    }
+  });
+
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
-
-// export const LoginSchema = RegisterSchema.required();
+export type UpdateSchemaType = z.infer<typeof UpdateSchema>;
