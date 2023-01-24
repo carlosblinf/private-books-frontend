ca8d3877dd918097c0968d853e623c7d7eee9e3a
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2023-01-24 00:45:59 -0500

    login form validated

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { From, TextInput } from '../../styled-components/forms';
+import { LoginSchema, LoginSchemaType } from '../../utils/validateForm';
 
 function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchemaType>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  function onsubmit(data: LoginSchemaType) {
+    console.log(data);
+  }
+
   return (
-    <From>
-      <TextInput type="text" placeholder="username" />
-      <TextInput type="password" placeholder="password" />
+    <From css={{ gap: '2rem' }} onSubmit={handleSubmit(onsubmit)}>
+      <div className="formGroup">
+        <TextInput
+          type="text"
+          placeholder="username"
+          {...register('userName')}
+          className={`form-control ${errors.userName ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.userName?.message}</div>
+      </div>
+      <div className="formGroup">
+        <TextInput
+          type="password"
+          placeholder="password"
+          {...register('password')}
+          className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+        />
+        <div className="invalid-feedback">{errors.password?.message}</div>
+      </div>
       <button className="w-2/5 p-3 text-xl text-white bg-gray-800 rounded" type="submit">
         Login
       </button>
--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -1,16 +1,24 @@
 import * as z from 'zod';
 
-export const RegisterSchema = z
-  .object({
-    fullName: z.string().min(5, { message: 'Full Name is required and more than 5' }),
-    userName: z.string().min(3, { message: 'UserName is required and more than 3' }),
-    email: z.string().email({ message: 'Invalid email address' }),
-    password: z.string().min(6, { message: 'Password is required and more than 6' }),
-    confirm_password: z.string().min(6, { message: 'Confirm Password' }),
-  })
-  .refine(({ confirm_password, password }) => password === confirm_password, {
-    message: "Passwords don't match",
-    path: ['confirm'], // path of error
-  });
+const schema = z.object({
+  fullName: z.string().min(5, { message: 'Full Name is required and more than 5' }),
+  userName: z.string().min(3, { message: 'UserName is required and more than 3' }),
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(6, { message: 'Password is required and more than 6' }),
+  confirm_password: z.string().min(6, { message: 'Confirm Password' }),
+});
+
+export const RegisterSchema = schema.refine(({ confirm_password, password }) => password === confirm_password, {
+  message: "Passwords don't match",
+  path: ['confirm'], // path of error
+});
+
+export const LoginSchema = schema.pick({
+  userName: true,
+  password: true,
+});
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
+export type LoginSchemaType = z.infer<typeof LoginSchema>;
+
+// export const LoginSchema = RegisterSchema.required();
